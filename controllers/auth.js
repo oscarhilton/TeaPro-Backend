057d89d34c4a122825e655410a02fb0181d5ebca
Oscar Hilton <oscar@Oscars-Air.home>
2017-10-27 23:44:02 +0100

    Changed back to old style JS, no babel

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,34 +7,39 @@ const User = mongoose.model('User');
 // Import Facebook and Google OAuth apps configs
 const keys = require('../config/keys');
 
-const transformFacebookProfile = (profile) => ({
-  oauth_id: profile.id,
-  name: profile.name,
-  avatar: profile.picture.data.url,
-});
+const transformFacebookProfile = function(profile){
+  return {
+    oauth_id: profile.id,
+    name: profile.name,
+    avatar: profile.picture.data.url
+  }
+};
 
 // Transform Google profile into user object
-const transformGoogleProfile = (profile) => ({
-  oauth_id: profile.id,
-  name: profile.displayName,
-  avatar: profile.image.url,
-});
+const transformGoogleProfile = function(profile){
+  return {
+    oauth_id: profile.id,
+    name: profile.displayName,
+    avatar: profile.image.url
+  }
+};
 
 // Register Facebook Passport strategy
 passport.use(new FacebookStrategy(keys.facebook,
   // Gets called when user authorizes access to their profile
-  async (accessToken, refreshToken, profile, done)
-    // Return done callback and pass transformed user object
-    => done(null, await createOrGetUserFromDatabase(transformFacebookProfile(profile._json)))
+  async function(accessToken, refreshToken, profile, done){
+    done(null, await createOrGetUserFromDatabase(transformFacebookProfile(profile._json)));
+  }
 ));
 
 // Register Google Passport strategy
 passport.use(new GoogleStrategy(keys.google,
-  async (accessToken, refreshToken, profile, done)
-    => done(null, await createOrGetUserFromDatabase(transformGoogleProfile(profile._json)))
+  async function(accessToken, refreshToken, profile, done){
+    done(null, await createOrGetUserFromDatabase(transformGoogleProfile(profile._json)));
+  }
 ));
 
-const createOrGetUserFromDatabase = async (userProfile) => {
+const createOrGetUserFromDatabase = async function(userProfile){
   let user = await User.findOne({ 'oauth_id': userProfile.oauth_id }).exec();
   if (!user) {
     user = new User({
@@ -48,10 +53,10 @@ const createOrGetUserFromDatabase = async (userProfile) => {
 };
 
 // Serialize user into the sessions
-passport.serializeUser((user, done) => done(null, user));
+passport.serializeUser(function(user, done){ done(null, user) });
 
 // Deserialize user from the sessions
-passport.deserializeUser((user, done) => done(null, user));
+passport.deserializeUser(function(user, done){ done(null, user) });
 
 module.exports = {
   facebookLogin: function() {
