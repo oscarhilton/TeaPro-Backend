487cea5188a5be62177985cec6e98f714eef30ea
Oscar Hilton <oscar@Oscars-Air.home>
2017-10-28 17:26:23 +0100

    Revert back to old auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,77 +1,67 @@
-// const passport = require('passport');
-// const FacebookStrategy = require('passport-facebook');
-// const GoogleStrategy = require('passport-google-oauth20');
-// const mongoose = require('mongoose');
-//
-// const User = mongoose.model('User');
-// // Import Facebook and Google OAuth apps configs
-// const keys = require('../config/keys');
-//
-// const transformFacebookProfile = function(profile){
-//   return {
-//     oauth_id: profile.id,
-//     name: profile.name,
-//     avatar: profile.picture.data.url
-//   }
-// };
-//
-// // Transform Google profile into user object
-// const transformGoogleProfile = function(profile){
-//   return {
-//     oauth_id: profile.id,
-//     name: profile.displayName,
-//     avatar: profile.image.url
-//   }
-// };
-//
-// // Register Facebook Passport strategy
-// passport.use(new FacebookStrategy(keys.facebook,
-//   // Gets called when user authorizes access to their profile
-//   async function(accessToken, refreshToken, profile, done){
-//     done(null, await createOrGetUserFromDatabase(transformFacebookProfile(profile._json)));
-//   }
-// ));
-//
-// // Register Google Passport strategy
-// passport.use(new GoogleStrategy(keys.google,
-//   async function(accessToken, refreshToken, profile, done){
-//     done(null, await createOrGetUserFromDatabase(transformGoogleProfile(profile._json)));
-//   }
-// ));
-//
-// const createOrGetUserFromDatabase = async function(userProfile){
-//   let user = await User.findOne({ 'oauth_id': userProfile.oauth_id }).exec();
-//   if (!user) {
-//     user = new User({
-//       oauth_id: userProfile.oauth_id,
-//       name: userProfile.name,
-//       avatar: userProfile.avatar,
-//     });
-//     await user.save();
-//   }
-//   return user;
-// };
-//
-// // Serialize user into the sessions
-// passport.serializeUser(function(user, done){ done(null, user) });
-//
-// // Deserialize user from the sessions
-// passport.deserializeUser(function(user, done){ done(null, user) });
-//
-// module.exports = {
-//   facebookLogin: function() {
-//     passport.authenticate('facebook');
-//   },
-//   facebookMiddleware: function() {
-//     passport.authenticate('facebook', { failureRedirect: '/auth/facebook' });
-//   },
-//   googleLogin: function() {
-//     googleLogin = passport.authenticate('google', { scope: ['profile'] });
-//   },
-//   googleMiddleware: function() {
-//     googleMiddleware = passport.authenticate('google', { failureRedirect: '/auth/google' });
-//   },
-//   oauthCallback: async function(req, res) {
-//     res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user));
-//   }
-// }
+import passport from 'passport';
+import FacebookStrategy from 'passport-facebook';
+import GoogleStrategy from 'passport-google-oauth20';
+import User from '../models/User';
+// Import Facebook and Google OAuth apps configs
+const keys = require('../config/keys');
+
+// Transform Facebook profile because Facebook and Google profile objects look different
+// and we want to transform them into user objects that have the same set of attributes
+const transformFacebookProfile = (profile) => ({
+  oauth_id: profile.id,
+  name: profile.name,
+  avatar: profile.picture.data.url,
+});
+
+// Transform Google profile into user object
+const transformGoogleProfile = (profile) => ({
+  oauth_id: profile.id,
+  name: profile.displayName,
+  avatar: profile.image.url,
+});
+
+// Register Facebook Passport strategy
+passport.use(new FacebookStrategy(keys.facebook,
+  // Gets called when user authorizes access to their profile
+  async (accessToken, refreshToken, profile, done)
+    // Return done callback and pass transformed user object
+    => done(null, await createOrGetUserFromDatabase(transformFacebookProfile(profile._json)))
+));
+
+// Register Google Passport strategy
+passport.use(new GoogleStrategy(keys.google,
+  async (accessToken, refreshToken, profile, done)
+    => done(null, await createOrGetUserFromDatabase(transformGoogleProfile(profile._json)))
+));
+
+const createOrGetUserFromDatabase = async (userProfile) => {
+  let user = await User.findOne({ 'oauth_id': userProfile.oauth_id }).exec();
+  if (!user) {
+    user = new User({
+      oauth_id: userProfile.oauth_id,
+      name: userProfile.name,
+      avatar: userProfile.avatar,
+    });
+    await user.save();
+  }
+  return user;
+};
+
+// Serialize user into the sessions
+passport.serializeUser((user, done) => done(null, user));
+
+// Deserialize user from the sessions
+passport.deserializeUser((user, done) => done(null, user));
+
+// Facebook
+export const facebookLogin = passport.authenticate('facebook');
+export const facebookMiddleware = passport.authenticate('facebook', { failureRedirect: '/auth/facebook' });
+
+// Google
+export const googleLogin = passport.authenticate('google', { scope: ['profile'] });
+export const googleMiddleware = passport.authenticate('google', { failureRedirect: '/auth/google' });
+
+// Callback
+export const oauthCallback = async (req, res) => {
+  res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user));
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,23 +6,25 @@ module.exports = app => {
   app.get('/auth/facebook',
           passport.authenticate('facebook'));
 
-  app.get('/auth/facebook/callback',
-          passport.authenticate('facebook'),
-          (req, res) => {
-            console.log(req, res);
-            res.redirect('/teas');
-            res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user));
-          }),
+  app.get(
+    '/auth/facebook/callback',
+    passport.authenticate('facebook'),
+    (req, res) => {
+      console.log(req, res);
+      res.redirect('/teas');
+      // res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user));
+    }),
 
   app.get('/auth/google',
           passport.authenticate('google', { scope: ['profile'] }));
 
-  app.get('/auth/google/callback',
-          passport.authenticate('google'),
-          (req, res) => {
-            res.redirect('/teas');
-            res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user));
-          }),
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user));
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
