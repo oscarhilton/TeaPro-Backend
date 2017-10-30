9a1a6ae6fb308b64364f99c470936bdf7c187533
Oscar Hilton <oscar@Oscars-Air.home>
2017-10-30 21:56:08 +0000

    New build

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ require('./routes/teaRoutes')(app);
 require('./routes/userRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 
   const path = require('path');
   app.get('*', (req, res) => {
