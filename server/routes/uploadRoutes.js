1ec9a468ae4fc6ba1e6953131a789d2152f6d10b
Oscar Hilton <oscar@Oscars-Air.lan>
2017-11-16 14:22:41 +0000

    Trying to get ios uploads working

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -46,9 +46,9 @@ module.exports = app => {
 
   app.post('/api/userupload', (req, res, next) => {
     // const { title, description } = req.body;
-    console.log(req);
     console.log(req.file);
     console.log(req.body);
+    res.send(req.file);
     // userUpload(req,res,function(err) {
   	// 	if(err) {
   	// 		return res.end('Error uploading file.');
